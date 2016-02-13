994c276628b4d4f203f189e86db43874f28a8696
Yuriy Chernyshov <thegeorg@yandex-team.ru>
2016-02-13 23:32:11 +0300

    Implemented (un)install targets

--- tools/bild/bild.mk
+++ tools/bild/bild.mk
@@ -28,9 +28,12 @@ $(_TMP_FILES)/%.o: %.cpp Makefile $(_TMP_FILES)
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 $(_DEPS_MK): $(SOURCES) $(_TMP_FILES)
+	#FIXME: this works improperly for interface files put into include/ folder
 	$(CXX) -MM $(SOURCES) > $@
 
+ifneq ($(SOURCES),)
 include $(_DEPS_MK)
+endif
 
 # Test execution targets
 _TEST_MODULES := $(TESTS:%.cpp=%)
@@ -54,6 +57,44 @@ endef
 
 $(foreach _TEST_MODULE,$(_TEST_MODULES),$(eval $(call _COMPILE_TEST_MODULE,$(_TEST_MODULE))))
 $(foreach _TEST_MODULE,$(_TEST_MODULES),$(eval $(call _RUN_TEST_MODULE,$(_TEST_MODULE))))
+
+# Install/Uninstall targets
+
+_LOCAL_FILES := $(foreach _FILE,$(FILES),$(shell basename $(_FILE)))
+
+install: $(_LOCAL_FILES)
+#Even though uninstall doesn't change local files,
+#it requires them to be built to uninstall folders properly
+uninstall: $(_LOCAL_FILES)
+
+install:
+	for DST_PATH in $(FILES); \
+	do \
+		SRC_PATH="`basename $$DST_PATH`"; \
+		DST_DIRNAME="`dirname $$DST_PATH`"; \
+		DST_PATH="$$DST_PATH"; \
+		\
+		mkdir -p "$$DST_DIRNAME"; \
+		cp -r "$$SRC_PATH" "$$DST_DIRNAME"; \
+	done \
+
+
+uninstall:
+	for DST_PATH in $(FILES); \
+	do \
+		SRC_PATH="`basename $$DST_PATH`"; \
+		DST_DIRNAME="`dirname $$DST_PATH`"; \
+		DST_PATH="$$DST_PATH"; \
+		if [ -d $$SRC_PATH ]; \
+		then \
+			for SINGLE_PATH in `find "$$SRC_PATH" -type f`; \
+			do \
+				rm -vf "$$DST_DIRNAME/$$SINGLE_PATH"; \
+			done; \
+		else \
+			rm -vf "$$DST_PATH"; \
+		fi; \
+	done \
 
 # Utility targets
 $(_TMP_FILES):
